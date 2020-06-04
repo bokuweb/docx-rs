7d2bf2a9ec941da2c29e88cf9ea92536a3e45020
bokuweb <bokuweb12@gmail.com>
2020-06-04 18:59:36 +0900

    fix: 0.0.77

--- docx-wasm/js/numbering.ts
+++ docx-wasm/js/numbering.ts
@@ -3,7 +3,7 @@ import { LevelOverride } from "./level";
 export class Numbering {
   id: number;
   abstractNumId: number;
-  overrides: LevelOverride[];
+  overrides: LevelOverride[] = [];
 
   constructor(id: number, abstractNumId: number) {
     this.id = id;

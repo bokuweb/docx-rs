a28df14f2add5b2c3a6adc4e71ffd99504b870eb
bokuweb <bokuweb12@gmail.com>
2020-10-05 18:16:13 +0900

    fix: improve lvlOverride to allow insert lvl

--- docx-core/src/documents/elements/level_override.rs
+++ docx-core/src/documents/elements/level_override.rs
@@ -1,3 +1,5 @@
+use super::*;
+
 use crate::documents::BuildXML;
 use crate::xml_builder::*;
 
@@ -5,22 +7,32 @@ use serde::Serialize;
 
 /*
 17.9.8 lvlOverride (Numbering Level Definition Override)
-This element specifies an optional override which shall be applied in place of zero or more levels from the abstract numbering definition for a given numbering definition instance. Each instance of this element is used to override the appearance and behavior of a given numbering level definition within the given abstract numbering definition.
+This element specifies an optional override which shall be applied in place of zero or more levels from the abstract numbering definition for a given numbering definition instance. Each instance of this element is used to override the appearance and behavior of a given numbering ilvl definition within the given abstract numbering definition.
 */
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct LevelOverride {
-    pub level: usize,
+    pub ilvl: usize,
     pub start: Option<usize>,
+    pub lvl: Option<Level>,
 }
 
 impl LevelOverride {
-    pub fn new(level: usize) -> LevelOverride {
-        LevelOverride { level, start: None }
+    pub fn new(ilvl: usize) -> LevelOverride {
+        LevelOverride {
+            ilvl,
+            start: None,
+            lvl: None,
+        }
     }
 
     pub fn start(mut self, start: usize) -> LevelOverride {
         self.start = Some(start);
+        self
+    }
+
+    pub fn level(mut self, lvl: Level) -> LevelOverride {
+        self.lvl = Some(lvl);
         self
     }
 }
@@ -28,7 +40,9 @@ impl LevelOverride {
 impl BuildXML for LevelOverride {
     fn build(&self) -> Vec<u8> {
         let mut b = XMLBuilder::new();
-        b = b.open_level_override(&format!("{}", self.level));
+        b = b.open_level_override(&format!("{}", self.ilvl));
+
+        b = b.add_optional_child(&self.lvl);
 
         if let Some(start) = self.start {
             b = b.start_override(&format!("{}", start));
@@ -57,38 +71,21 @@ mod tests {
 </w:lvlOverride>"#
         );
     }
-}
 
-// Example
-/*
-<w:num w:numId="5">
-  <w:abstractNumId w:val="0"/>
-  <w:lvlOverride w:ilvl="0">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="1">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="2">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="3">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="4">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="5">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="6">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="7">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-  <w:lvlOverride w:ilvl="8">
-    <w:startOverride w:val="1"/>
-  </w:lvlOverride>
-</w:num>
-*/
+    #[test]
+    fn test_override_with_lvl() {
+        let lvl = Level::new(
+            1,
+            Start::new(1),
+            NumberFormat::new("decimal"),
+            LevelText::new("%4."),
+            LevelJc::new("left"),
+        );
+        let c = LevelOverride::new(1).level(lvl);
+        let b = c.build();
+        assert_eq!(
+            str::from_utf8(&b).unwrap(),
+            r#"<w:lvlOverride w:ilvl="1"><w:lvl w:ilvl="1"><w:start w:val="1" /><w:numFmt w:val="decimal" /><w:lvlText w:val="%4." /><w:lvlJc w:val="left" /><w:pPr><w:rPr /></w:pPr></w:lvl></w:lvlOverride>"#
+        );
+    }
+}

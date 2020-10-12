425e2bce08e79ea03668297ea53f34fcfe93354f
bokuweb <bokuweb12@gmail.com>
2020-10-13 00:04:09 +0900

    Add page margin test (#175)
    * fix: page margin
    
    * fix: test
    
    * 0.0.109

--- docx-core/examples/page_margin.rs
+++ docx-core/examples/page_margin.rs
@@ -5,13 +5,6 @@ pub fn main() -> Result<(), DocxError> {
     let file = std::fs::File::create(&path).unwrap();
     Docx::new()
         .add_paragraph(Paragraph::new().add_run(Run::new().add_text("Hello")))
-        .page_margin(
-            PageMargin::new()
-                .top(3200)
-                .footer(3200)
-                .left(3200)
-                .right(3200),
-        )
         .build()
         .pack(file)?;
     Ok(())

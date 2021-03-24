053fdd8a7fd4c7e534890800c15af990fe38defa
bokuweb <bokuweb12@gmail.com>
2021-03-24 17:09:50 +0900

    0.0.177

--- docx-core/src/reader/attributes/border.rs
+++ docx-core/src/reader/attributes/border.rs
@@ -16,8 +16,8 @@ pub struct BorderAttrs {
 pub fn read_border(attrs: &[OwnedAttribute]) -> Result<BorderAttrs, ReaderError> {
     let mut border_type = BorderType::Single;
     let mut color = "000000".to_owned();
-    let mut size: Option<u32> = None;
-    let mut space: Option<u32> = None;
+    let mut size: Option<u32> = Some(4);
+    let mut space: Option<u32> = Some(0);
     dbg!(&attrs);
     for a in attrs {
         let local_name = &a.name.local_name;

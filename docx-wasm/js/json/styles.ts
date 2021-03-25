db70bacb17db284fce225611b991eed103cb551f
bokuweb <bokuweb12@gmail.com>
2021-03-25 14:58:50 +0900

    fix: spec

--- docx-wasm/js/json/styles.ts
+++ docx-wasm/js/json/styles.ts
@@ -1,6 +1,7 @@
 import { RunPropertyJSON } from "./run";
 import { ParagraphPropertyJSON } from "./paragraph";
 import { TablePropertyJSON } from "./table";
+import { TableCellPropertyJSON } from "..";
 
 export type StyleJSON = {
   styleId: string;

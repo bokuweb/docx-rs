5539df0e2712d0e4b984a6a55a83eb60506e2d80
bokuweb <bokuweb12@gmail.com>
2021-07-09 12:06:23 +0900

    fix:

--- docx-wasm/js/webextension.ts
+++ docx-wasm/js/webextension.ts
@@ -1,5 +1,6 @@
 export class WebExtension {
   _id: string;
+  _referenceId: string;
   _version: string;
   _store: string;
   _storeType: string;
@@ -7,8 +8,15 @@ export class WebExtension {
     [k: string]: string;
   } = {};
 
-  constructor(id: string, version: string, store: string, storeType: string) {
+  constructor(
+    id: string,
+    referenceId: string,
+    version: string,
+    store: string,
+    storeType: string
+  ) {
     this._id = id;
+    this._referenceId = referenceId;
     this._version = version;
     this._store = store;
     this._storeType = storeType;
--- docx-wasm/src/web_extension.rs
+++ docx-wasm/src/web_extension.rs
@@ -7,11 +7,18 @@ pub struct WebExtension(docx_rs::WebExtension);
 #[wasm_bindgen(js_name = createWebExtension)]
 pub fn create_web_extension(
     id: &str,
+    reference_id: &str,
     version: &str,
     store: &str,
     store_type: &str,
 ) -> WebExtension {
-    WebExtension(docx_rs::WebExtension::new(id, version, store, store_type))
+    WebExtension(docx_rs::WebExtension::new(
+        id,
+        reference_id,
+        version,
+        store,
+        store_type,
+    ))
 }
 
 impl WebExtension {

fde04df5f377a78d43517f7fb553a83b60504a6c
bokuweb <bokuweb12@gmail.com>
2021-11-25 17:59:40 +0900

    fix: reader and testing

--- docx-core/src/reader/paragraph_property.rs
+++ docx-core/src/reader/paragraph_property.rs
@@ -40,28 +40,9 @@ impl ElementReader for ParagraphProperty {
                             continue;
                         }
                         XMLElement::Spacing => {
-                            if let Ok((before, after, line, spacing_type)) =
+                            if let Ok(spacing) =
                                 attributes::line_spacing::read_line_spacing(&attributes)
                             {
-                                dbg!(before, after, line, spacing_type);
-                                let mut spacing = LineSpacing::new();
-
-                                if let Some(spacing_type) = spacing_type {
-                                    spacing = spacing.line_rule(spacing_type);
-                                };
-
-                                if let Some(before) = before {
-                                    spacing = spacing.before(before);
-                                }
-
-                                if let Some(after) = after {
-                                    spacing = spacing.after(after);
-                                }
-
-                                if let Some(line) = line {
-                                    spacing = spacing.line(line);
-                                }
-
                                 p = p.line_spacing(spacing);
                             }
                             continue;

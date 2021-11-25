969225c8409fd99b3c89491c22b58d35ab0d2937
bokuweb <bokuweb12@gmail.com>
2021-11-25 23:19:53 +0900

    fix: disabled empty header

--- docx-core/examples/header.rs
+++ docx-core/examples/header.rs
@@ -3,8 +3,10 @@ use docx_rs::*;
 pub fn main() -> Result<(), DocxError> {
     let path = std::path::Path::new("./output/header.docx");
     let file = std::fs::File::create(&path).unwrap();
+    let header =
+        Header::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Hello")));
     Docx::new()
-        .add_header_paragraph(Paragraph::new().add_run(Run::new().add_text("Hello")))
+        .header(header)
         .add_paragraph(Paragraph::new().add_run(Run::new().add_text("World")))
         .build()
         .pack(file)?;

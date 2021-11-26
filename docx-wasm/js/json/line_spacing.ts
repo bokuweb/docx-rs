2fdc761c960386376e84334efd5e62d7129d2c0a
bokuweb <bokuweb12@gmail.com>
2021-11-26 18:55:22 +0900

    fix: def

--- docx-wasm/js/json/line_spacing.ts
+++ docx-wasm/js/json/line_spacing.ts
@@ -1,9 +1,11 @@
 export type LineSpacingJSON = {
-    before: number | null;
-    after: number | null;
-    line: number | null;
-    lineRule: {
-        type: "atLeast" | "auto" | "exact";
-        val: number;
-    } | null;
+  before?: number | null;
+  after?: number | null;
+  beforeLines?: number | null;
+  afterLines?: number | null;
+  line?: number | null;
+  lineRule?: {
+    type: "atLeast" | "auto" | "exact";
+    val: number;
+  } | null;
 };

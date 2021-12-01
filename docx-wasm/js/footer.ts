1369f5435c6410a5d89c5f8dbf85c9e156a6d2a0
bokuweb <bokuweb12@gmail.com>
2021-12-02 00:31:50 +0900

    fix: Add test and support table

--- docx-wasm/js/footer.ts
+++ docx-wasm/js/footer.ts
@@ -1,14 +1,23 @@
 import { Paragraph } from "./paragraph";
+import { Table } from "./table";
 
 export class Footer {
   hasNumberings = false;
-  children: Paragraph[] = [];
+  children: (Paragraph | Table)[] = [];
 
   addParagraph(p: Paragraph) {
     if (p.hasNumberings) {
       this.hasNumberings = true;
     }
     this.children.push(p);
+    return this;
+  }
+
+  addTable(t: Table) {
+    if (t.hasNumberings) {
+      this.hasNumberings = true;
+    }
+    this.children.push(t);
     return this;
   }
 }

64e30fca792ef507b8c14e01398385dbf37198d7
git-noise <63017366+git-noise@users.noreply.github.com>
2024-06-27 15:43:47 -0400

    Initial positional tab support: add ptab()

--- docx-core/src/documents/elements/run_property.rs
+++ docx-core/src/documents/elements/run_property.rs
@@ -48,6 +48,8 @@ pub struct RunProperty {
     pub ins: Option<Insert>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub strike: Option<Strike>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub positional_tab: Option<PositionalTab>,
 }
 
 impl RunProperty {
@@ -154,6 +156,11 @@ impl RunProperty {
         self.ins = Some(i);
         self
     }
+
+    pub fn ptab(mut self, ptab: PositionalTab) -> Self {
+        self.positional_tab = Some(ptab);
+        self
+    }
 }
 
 impl BuildXML for RunProperty {
@@ -180,6 +187,7 @@ impl BuildXML for RunProperty {
             .add_optional_child(&self.vert_align)
             .add_optional_child(&self.character_spacing)
             .add_optional_child(&self.style)
+            .add_optional_child(&self.positional_tab)
             .close()
             .build()
     }
@@ -262,6 +270,7 @@ mod tests {
             r#"<w:rPr><w:rFonts w:eastAsia="Hiragino" /></w:rPr>"#
         );
     }
+
     #[test]
     fn test_character_spacing() {
         let c = RunProperty::new().spacing(20);
@@ -269,6 +278,20 @@ mod tests {
         assert_eq!(
             str::from_utf8(&b).unwrap(),
             r#"<w:rPr><w:spacing w:val="20" /></w:rPr>"#
+        );
+    }
+
+    #[test]
+    fn test_ptab() {
+        let c = RunProperty::new().ptab(PositionalTab::new(
+            PositionalTabAlignmentType::Left,
+            PositionalTabRelativeTo::Margin,
+            TabLeaderType::None,
+        ));
+        let b = c.build();
+        assert_eq!(
+            str::from_utf8(&b).unwrap(),
+            r#"<w:rPr><w:ptab w:alignment="left" w:relativeTo="margin" w:leader="none" /></w:rPr>"#
         );
     }
 }

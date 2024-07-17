72ffd264b24626fe44d375faa62b97fa450b0146
bokuweb <bokuweb12@gmail.com>
2024-07-17 17:38:26 +0900

    fix

--- docx-core/src/reader/paragraph_property.rs
+++ docx-core/src/reader/paragraph_property.rs
@@ -95,9 +95,8 @@ impl ElementReader for ParagraphProperty {
                             continue;
                         }
                         XMLElement::SnapToGrid => {
-                            if read_bool(&attributes) {
-                                p.snap_to_grid = Some(true);
-                            }
+                            let v = read_bool(&attributes);
+                            p.snap_to_grid = Some(v);
                         }
                         XMLElement::KeepNext => {
                             if read_bool(&attributes) {

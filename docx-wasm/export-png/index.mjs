14429a1a266cce35db762140863cd81b5c294492
bokuweb <bokuweb12@gmail.com>
2024-11-01 12:54:06 +0900

    fix

--- docx-wasm/export-png/index.mjs
+++ docx-wasm/export-png/index.mjs
@@ -1,8 +1,8 @@
-import fs from 'fs';
-import cp from 'child_process';
-import path from 'path';
+import fs from "fs";
+import cp from "child_process";
+import path from "path";
 
-const fileType = 'png';
+const fileType = "png";
 
 const inputDir = process.env.OUTPUT_DIR ?? process.exit(1);
 const files = fs.readdirSync(inputDir);
@@ -10,10 +10,14 @@ const files = fs.readdirSync(inputDir);
 for (const file of files) {
   if (/\.docx$/.test(file)) {
     console.log(file);
-    const spawn = cp.spawnSync('make', [
-      'run',
+    const spawn = cp.spawnSync("make", [
+      "run",
       `INPUT=${path.join(inputDir, file)}`,
-      `OUTPUT=${path.join(__dirname, 'png', file.replace(/\.docx$/, `.${fileType}`))}`,
+      `OUTPUT=${path.join(
+        __dirname,
+        "png",
+        file.replace(/\.docx$/, `.${fileType}`)
+      )}`,
     ]);
     console.log(spawn.stdout.toString());
     //break;

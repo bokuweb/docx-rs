69cb890179ebf02222c1e282a93647090ffaa74c
xamgore <xamgore@ya.ru>
2024-11-02 18:35:19 +0300

    Expose the BuildXML trait without displaying its methods in hints

--- docx-core/src/documents/build_xml.rs
+++ docx-core/src/documents/build_xml.rs
@@ -3,11 +3,13 @@ use std::io::Write;
 
 pub trait BuildXML {
     /// Write XML to the output stream.
+    #[doc(hidden)]
     fn build_to<W: Write>(
         &self,
         stream: xml::writer::EventWriter<W>,
     ) -> xml::writer::Result<xml::writer::EventWriter<W>>;
 
+    #[doc(hidden)]
     fn build(&self) -> Vec<u8> {
         self.build_to(XMLBuilder::new(Vec::new()).into_inner().unwrap())
             .expect("should write to buf")

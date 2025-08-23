82e6cb7cd8ea3a7a5db9ece5e8d711e5b4831d2d
bokuweb <bokuweb12@gmail.com>
2025-08-23 18:08:50 +0900

    impl: section

--- docx-core/src/documents/elements/section.rs
+++ docx-core/src/documents/elements/section.rs
@@ -1,19 +1,135 @@
 use super::*;
 use crate::documents::BuildXML;
+use crate::types::*;
 use crate::xml_builder::*;
+use crate::{delegate_getters_to_field, delegate_to_field, Footer, Header};
+use serde::ser::{SerializeStruct, Serializer};
+use serde::Serialize;
 use std::io::Write;
 
-use serde::Serialize;
+#[derive(Debug, Clone, PartialEq)]
+pub enum SectionChild {
+    Paragraph(Box<Paragraph>),
+    Table(Box<Table>),
+    BookmarkStart(BookmarkStart),
+    BookmarkEnd(BookmarkEnd),
+    CommentStart(Box<CommentRangeStart>),
+    CommentEnd(CommentRangeEnd),
+    StructuredDataTag(Box<StructuredDataTag>),
+    TableOfContents(Box<TableOfContents>),
+}
+
+impl Serialize for SectionChild {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            SectionChild::Paragraph(ref p) => {
+                let mut t = serializer.serialize_struct("Paragraph", 2)?;
+                t.serialize_field("type", "paragraph")?;
+                t.serialize_field("data", p)?;
+                t.end()
+            }
+            SectionChild::Table(ref c) => {
+                let mut t = serializer.serialize_struct("Table", 2)?;
+                t.serialize_field("type", "table")?;
+                t.serialize_field("data", c)?;
+                t.end()
+            }
+            SectionChild::BookmarkStart(ref c) => {
+                let mut t = serializer.serialize_struct("BookmarkStart", 2)?;
+                t.serialize_field("type", "bookmarkStart")?;
+                t.serialize_field("data", c)?;
+                t.end()
+            }
+            SectionChild::BookmarkEnd(ref c) => {
+                let mut t = serializer.serialize_struct("BookmarkEnd", 2)?;
+                t.serialize_field("type", "bookmarkEnd")?;
+                t.serialize_field("data", c)?;
+                t.end()
+            }
+            SectionChild::CommentStart(ref r) => {
+                let mut t = serializer.serialize_struct("CommentRangeStart", 2)?;
+                t.serialize_field("type", "commentRangeStart")?;
+                t.serialize_field("data", r)?;
+                t.end()
+            }
+            SectionChild::CommentEnd(ref r) => {
+                let mut t = serializer.serialize_struct("CommentRangeEnd", 2)?;
+                t.serialize_field("type", "commentRangeEnd")?;
+                t.serialize_field("data", r)?;
+                t.end()
+            }
+            SectionChild::StructuredDataTag(ref r) => {
+                let mut t = serializer.serialize_struct("StructuredDataTag", 2)?;
+                t.serialize_field("type", "structuredDataTag")?;
+                t.serialize_field("data", r)?;
+                t.end()
+            }
+            SectionChild::TableOfContents(ref r) => {
+                let mut t = serializer.serialize_struct("TableOfContents", 2)?;
+                t.serialize_field("type", "tableOfContents")?;
+                t.serialize_field("data", r)?;
+                t.end()
+            }
+        }
+    }
+}
+
+impl BuildXML for SectionChild {
+    fn build_to<W: Write>(
+        &self,
+        stream: xml::writer::EventWriter<W>,
+    ) -> xml::writer::Result<xml::writer::EventWriter<W>> {
+        match self {
+            SectionChild::Paragraph(v) => v.build_to(stream),
+            SectionChild::Table(v) => v.build_to(stream),
+            SectionChild::BookmarkStart(v) => v.build_to(stream),
+            SectionChild::BookmarkEnd(v) => v.build_to(stream),
+            SectionChild::CommentStart(v) => v.build_to(stream),
+            SectionChild::CommentEnd(v) => v.build_to(stream),
+            SectionChild::StructuredDataTag(v) => v.build_to(stream),
+            SectionChild::TableOfContents(v) => v.build_to(stream),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Section {
     property: SectionProperty,
+    children: Vec<SectionChild>,
 }
 
 impl Section {
     pub fn new() -> Section {
         Default::default()
+    }
+
+    delegate_to_field! {
+        property =>
+        page_size(size: PageSize) -> Self,
+        page_margin(margin: PageMargin) -> Self,
+        page_orient(o: PageOrientationType) -> Self,
+        doc_grid(doc_grid: DocGrid) -> Self,
+        text_direction(direction: String) -> Self,
+        title_pg() -> Self,
+        header(h: Header, rid: &str) -> Self,
+        first_header(h: Header, rid: &str) -> Self,
+        first_header_without_title_pg(h: Header, rid: &str) -> Self,
+        even_header(h: Header, rid: &str) -> Self,
+        footer(h: Footer, rid: &str) -> Self,
+        first_footer(h: Footer, rid: &str) -> Self,
+        first_footer_without_title_pg(h: Footer, rid: &str) -> Self,
+        even_footer(h: Footer, rid: &str) -> Self,
+        page_num_type(h: PageNumType) -> Self,
+    }
+
+    delegate_getters_to_field! {
+        property =>
+        get_headers() -> Vec<&Header>,
+        get_footers() -> Vec<&Footer>,
     }
 }
 
@@ -21,6 +137,7 @@ impl Default for Section {
     fn default() -> Self {
         Self {
             property: SectionProperty::new(),
+            children: vec![],
         }
     }
 }
@@ -32,6 +149,7 @@ impl BuildXML for Section {
     ) -> xml::writer::Result<xml::writer::EventWriter<W>> {
         let id = crate::generate_para_id();
         XMLBuilder::from(stream)
+            .add_children(&self.children)?
             .open_paragraph(&id)?
             .open_paragraph_property()?
             .add_child(&self.property)?

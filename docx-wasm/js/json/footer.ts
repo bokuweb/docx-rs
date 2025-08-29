98e3bd7e81d47fc95fba796ccd983f82b399b186
bokuweb <bokuweb12@gmail.com>
2025-08-30 01:33:58 +0900

    fix: support footer in section

--- docx-wasm/js/json/footer.ts
+++ docx-wasm/js/json/footer.ts
@@ -2,9 +2,12 @@ import { ParagraphJSON } from "./paragraph";
 import { StructuredTagJSON } from "./structured-data-tag";
 import { TableJSON } from "./table";
 
-export type FooterJSON = {
-  children: (ParagraphJSON | TableJSON | StructuredTagJSON)[];
-};
+export type FooterJSON = [
+  string,
+  {
+    children: (ParagraphJSON | TableJSON | StructuredTagJSON)[];
+  }
+];
 
 export type FooterReferenceJSON = {
   footerType: string;

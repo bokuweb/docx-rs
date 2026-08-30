b91c6978b7994e6f4e4e53ffe39aaddd722f54fd
Yonghye Kwon <developer.0hye@gmail.com>
2026-08-30 13:45:00 +0700

    fix: preserve parsed picture rotation
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- docx-core/src/reader/pic.rs
+++ docx-core/src/reader/pic.rs
@@ -24,6 +24,19 @@ impl ElementReader for Pic {
                                     pic = pic.id(id)
                                 }
                             }
+                            AXMLElement::Xfrm => {
+                                if let Some(rotation) = read(&attributes, "rot") {
+                                    if let Ok(rotation) = i64::from_str(&rotation) {
+                                        const UNITS_PER_DEGREE: i64 = 60_000;
+                                        const FULL_TURN_UNITS: i64 = 360 * UNITS_PER_DEGREE;
+                                        let normalized = rotation.rem_euclid(FULL_TURN_UNITS);
+                                        let degrees = ((normalized + UNITS_PER_DEGREE / 2)
+                                            / UNITS_PER_DEGREE)
+                                            % 360;
+                                        pic = pic.rotate(degrees as u16);
+                                    }
+                                }
+                            }
                             AXMLElement::Off => {
                                 let mut offset_x: i32 = 0;
                                 let mut offset_y: i32 = 0;
@@ -68,5 +81,28 @@ impl ElementReader for Pic {
                 _ => {}
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reads_picture_rotation_from_transform() {
+        let xml = r#"<pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture" xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+            <pic:blipFill><a:blip r:embed="rId1"/></pic:blipFill>
+            <pic:spPr>
+                <a:xfrm rot="20933656">
+                    <a:off x="0" y="0"/>
+                    <a:ext cx="4366365" cy="4366365"/>
+                </a:xfrm>
+            </pic:spPr>
+        </pic:pic>"#;
+        let mut reader = EventReader::new(xml.as_bytes());
+
+        let picture = Pic::read(&mut reader, &[]).expect("picture should parse");
+
+        assert_eq!(picture.rot, 349);
     }
 }
